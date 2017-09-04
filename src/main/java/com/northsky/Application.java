0ab70fa52591ba584eb4b8200990896aeff93f6d
jogging520 <446734796@qq.com>
2017-09-04 12:13:08 +0800

    修改成xml  测试报错

--- src/main/java/com/northsky/Application.java
+++ src/main/java/com/northsky/Application.java
@@ -1,11 +1,13 @@
 package com.northsky;
 
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@MapperScan("com.northsky.dao")  
 public class Application {
 
 	public static void main(String[] args) {
---
+++ src/test/java/MybatisXMLTest.java
@@ -0,0 +1,35 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.northsky.Application;
+import com.northsky.dao.StationInformationPOMapper;
+import com.northsky.model.po.StationInformationPO;
+import com.northsky.service.PlayListService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@Transactional
+public class MybatisXMLTest {
+	
+//	@Autowired
+//	private PlayListService  playService;
+	@Autowired
+	StationInformationPOMapper stationmapper;
+	@Test
+	public void test() {
+		 StationInformationPO stationPO = (StationInformationPO) stationmapper.selectByPrimaryKey(1);
+		 System.out.println(stationPO.getCategory());
+		
+//		ChannelInformationPO cpo =channelmapper.findAll();
+//		 System.out.println(cpo.getName());
+//		Media media = mediaMapper.findByid(100000);
+//		PlayListVO playvo =playService.getPlaylist();
+//		System.out.println(playvo.getStations().get(1).getCategory());
+//		Assert.assertEquals("../images/guangdian/lvyou-heilongjiang.png",media.getLocation());
+	}
+
+}
