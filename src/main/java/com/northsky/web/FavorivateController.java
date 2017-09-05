848bebc282d05124a048d75b1237acd8b61ffabd
jogging520 <446734796@qq.com>
2017-09-05 09:11:39 +0800

    开始服务集成！

---
+++ src/main/java/com/northsky/web/FavorivateController.java
@@ -0,0 +1,39 @@
+package com.northsky.web;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.northsky.model.vo.FavouriteRecordVO;
+import com.northsky.service.FavouriteService;
+@Controller
+public class FavorivateController {
+	@Autowired
+	private FavouriteService favouriteService;
+	private Logger logger = Logger.getLogger(getClass());
+	
+	@RequestMapping(value="/getFavoriateAll",method = RequestMethod.GET )
+    @ResponseBody
+    public List<JSONObject>  getFavoriteAll(){
+		logger.info(System.currentTimeMillis());
+		List<FavouriteRecordVO> favourites = favouriteService.getMedia(88888888, "image");
+		List<JSONObject> jaons =new ArrayList<JSONObject>();
+		for(FavouriteRecordVO favouriteVO :favourites){
+			JSONObject favorJason = (JSONObject) JSON.toJSON(favouriteVO);
+			jaons.add(favorJason);
+		}
+		
+		logger.info(System.currentTimeMillis());
+		return jaons;
+    }
+	
+	
+}
---
+++ src/main/java/com/northsky/web/MediaController.java
@@ -0,0 +1,31 @@
+package com.northsky.web;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.northsky.model.vo.MediaInformationVO;
+import com.northsky.service.MediaService;
+@Controller
+public class MediaController {
+	@Autowired
+	private MediaService MediaService;
+	private Logger logger = Logger.getLogger(getClass());
+	
+	@RequestMapping(value="/getMediaAll/{{media_id}}",method = RequestMethod.GET )
+    @ResponseBody
+    public JSONObject getMedia(@PathVariable int media_id){
+		MediaInformationVO mediaInformationVO= MediaService.getMedia(media_id);
+		JSONObject mediaJason = (JSONObject) JSON.toJSON(mediaInformationVO);
+		return  mediaJason;
+		
+	}
+	
+	
+}
